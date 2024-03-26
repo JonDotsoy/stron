d48dd24ad6120267c2534873ef16d4eb6fab72e0
Jonathan Delgado <hi@jon.soy>
2024-03-26 10:00:36 -0300

    tests(stron): add new tests for `stron` package and remove old test file
    This commit introduces a new set of test cases for the `stron` package in the form of `index.spec.js`, adapted to use the `bun:test` framework. These tests aim to validate the conversion behavior of various inputs to boolean true or false, reflecting the package's purpose more accurately. The old test file `spec.js`, which relied on the `chai` assertion library and did not align with the updated testing strategy, has been removed to maintain consistency and avoid redundant testing across different frameworks. This change allows for a more streamlined and modern approach to testing within the project, leveraging `bun:test`'s capabilities for better test readability and efficiency.

---
+++ packages/stron/test/index.spec.js
@@ -0,0 +1,38 @@
+const { test, expect } = require('bun:test')
+const { inspect } = require('util')
+const stron = require('../index.js')
+
+
+const variablesTrue = [
+  true,
+  'true',
+  'on',
+  'active',
+  'enable',
+  'enabled',
+  1,
+  '1',
+  'activate',
+  'activated'
+]
+
+const fakeVariables = [
+  false,
+  'off',
+  'false',
+  'inactive',
+  0,
+]
+
+for (const variableTrue of variablesTrue) {
+  test(`check: ${inspect(variableTrue, { colors: true })} equal to ${inspect(true, { colors: true })}`, function () {
+    expect(stron(variableTrue)).toEqual(true)
+  })
+}
+
+for (const variableTrue of fakeVariables) {
+  test(`check: ${inspect(variableTrue, { colors: true })} not equal to ${inspect(true, { colors: true })}`, function () {
+    expect(stron(variableTrue)).toEqual(false)
+  })
+}
+
